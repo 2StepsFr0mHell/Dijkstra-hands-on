7a77d1b0cf57f57b7110af37d4524807554619de
2StepsFr0mHell <2Steps@MacBook-Thibaud.local>
2017-06-15 08:46:24 +0200

    create graph
    remove comments

--- python-project/create_graph.py
+++ python-project/create_graph.py
@@ -7,52 +7,8 @@ class Vertex:
     def add_succ(self, successor, distance):
         self.successors.append((successor, distance))   # tuple (immutable)
 
-    '''
-    def printNeighbours(self):
-        print("Neighbours of %s"%self.name)
-        for s in self.successors:
-            print("\t%s %d"%(s[0], s[1]))
-    '''
 
 def createGraph(vlist, elist):
     graph= {}   # dictionnary
-    '''
-    for v in vlist:
-        edges[v] = Edge(v)
-    for e in edges:
-        print(e)
-
-    for e in elist:
-        edges[e[0]].add_succ(e[1], e[2])
-        edges[e[1]].add_succ(e[0], e[2])
-
-    for v in graph:
-        graph[v].printNeighbours()
-    '''
+    # @TODO
     return graph
-
-
-'''
-def main():    
-    # Graph definition
-    vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    # Attention : undirected graph
-    edges = [['A', 'B', 5],
-             ['A', 'D', 3],
-             ['A', 'E', 12],
-             ['A', 'F', 5],
-             ['B', 'D', 1],
-             ['B', 'G', 2],
-             ['C', 'E', 1],
-             ['C', 'F', 16],
-             ['D', 'E', 1],
-             ['D', 'G', 1],
-             ['E', 'F', 2]
-             ]
-    nodes= createGraph(vertices, edges)
-
-
-
-if __name__ == '__main__':
-    main()
-'''    
